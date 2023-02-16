a7dc11a0cbb8c9f34f9d2bfade3030da299a20e8
EuRV <eugenirovensky@yandex.ru>
2023-02-16 14:59:36 +0600

    feat: add localization in russian language

---
+++ src/locales/ru.js
@@ -0,0 +1,28 @@
+export default {
+  translation: {
+    content: {
+      body: {
+        h_one: 'RSS агрегатор',
+        p_lead: 'Начните читать RSS сегодня! Это легко, это красиво.',
+        p_example: 'Пример: https://ru.hexlet.io/lessons.rss',
+        lable_url_input: 'Ссылка RSS',
+        btn_primary: 'Добавить',
+      },
+      modal: {
+        btn_secondary: 'Закрыть',
+        a_btn_primary: 'Читать полностью',
+        feeds: 'Фиды',
+        posts: 'Посты',
+      },
+      footer: {
+        a: 'Hexlet',
+        div: 'created by',
+      },
+    },
+    errors: {
+      invalidURL: 'Ссылка должна быть валидным URL',
+      existingURL: 'RSS уже существует',
+      successURL: 'RSS успешно загружен',
+    },
+  },
+};
