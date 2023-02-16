7c6bb496e8eed2a9e0eef342b87fd25303d508b6
EuRV <eugenirovensky@yandex.ru>
2023-02-17 01:40:32 +0600

    feat: implement rendering of posts and channels

--- src/view.js
+++ src/view.js
@@ -6,6 +6,74 @@ const renderErrors = (elements, errors) => {
   elements.feedback.textContent = errorMessage;
 };
 
+const renderFeeds = (elements, i18n, feeds) => {
+  const feedsEl = elements.rss.feeds;
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = i18n.t('content.modal.feeds');
+  const listGroop = document.createElement('ul');
+  listGroop.classList.add('list-group', 'border-0', 'rounded-0');
+  feeds.forEach((feed) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = feed.title;
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = feed.description;
+    li.append(h3, p);
+    listGroop.prepend(li);
+  });
+
+  cardBody.append(cardTitle);
+  card.append(cardBody, listGroop);
+  feedsEl.innerHTML = '';
+  feedsEl.append(card);
+};
+
+const renderPosts = (elements, i18n, posts) => {
+  const postsEl = elements.rss.posts;
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = i18n.t('content.modal.posts');
+  const listGroop = document.createElement('ul');
+  listGroop.classList.add('list-group', 'border-0', 'rounded-0');
+  posts.forEach((post) => {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const a = document.createElement('a');
+    a.setAttribute('href', `${post.link}`);
+    a.classList.add('fw-bold');
+    a.setAttribute('data-id', `${post.id}`);
+    a.setAttribute('target', '_blank');
+    a.setAttribute('rel', 'noopener noreferrer');
+    a.textContent = post.title;
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-id', `${post.id}`);
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+    button.textContent = i18n.t('content.modal.btn_modal');
+    li.append(a, button);
+    listGroop.prepend(li);
+  });
+
+  cardBody.append(cardTitle);
+  card.append(cardBody, listGroop);
+  postsEl.innerHTML = '';
+  postsEl.append(card);
+};
+
 const render = (elements, i18n) => {
   elements.input.classList.remove('is-invalid');
   elements.feedback.classList.remove('text-danger');
@@ -17,8 +85,15 @@ const processHandler = (elements, process, i18n) => {
   switch (process) {
     case 'success':
       render(elements, i18n);
+      elements.input.disabled = false;
+      elements.submit.disabled = false;
       elements.form.reset();
       elements.input.focus();
+      break;
+
+    case 'sending':
+      elements.input.disabled = true;
+      elements.submit.disabled = true;
       break;
 
     default:
@@ -37,12 +112,21 @@ export const initView = (elements, i18n) => (path, value) => {
       processHandler(elements, value, i18n);
       break;
 
+    case 'feeds':
+      renderFeeds(elements, i18n, value);
+      break;
+
+    case 'posts':
+      renderPosts(elements, i18n, value);
+      break;
+
     default:
       break;
   }
 };
 
 export const renderContent = (elements, i18n) => {
+  // console.log(Object.entries(elements.content))
   const { body, modal, footer } = elements.content;
   Object.entries(body).forEach(([key, value]) => {
     const element = value;
