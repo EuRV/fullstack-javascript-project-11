e7d65b1b7608b52df0645cfb80158c7d2a195a12
EuRV <eugenirovensky@yandex.ru>
2023-02-17 01:42:51 +0600

    refactor: remove duplication

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,40 +1,20 @@
-const mapping = {
-  feeds: {
-    title: (el) => (key) => el.querySelector(`channel > ${key}`).textContent,
-    description: (el) => (key) => el.querySelector(`channel > ${key}`).textContent,
-  },
-  posts: {
-    title: (el) => (key) => el.querySelector(key).textContent,
-    description: (el) => (key) => el.querySelector(key).textContent,
-    link: (el) => (key) => el.querySelector(key).textContent,
-  },
-};
-
-const patern = {
-  feeds: [
-    'title',
-    'description',
-  ],
-  posts: [
-    'title',
-    'description',
-    'link',
-  ],
-};
-
 export default (page) => {
   const parser = new DOMParser();
   const domFromXML = parser.parseFromString(page, 'text/xml');
+  const titleChannel = domFromXML.querySelector('channel > title').textContent;
+  const descriptionChannel = domFromXML.querySelector('channel > description').textContent;
   const items = domFromXML.getElementsByTagName('item');
-  const feeds = patern.feeds
-    .reduce((acc, tagName) => (
-      { ...acc, [tagName]: mapping.feeds[tagName](domFromXML)(tagName) }
-    ), {});
+
+  const feed = {
+    title: titleChannel,
+    description: descriptionChannel,
+  };
   const posts = Array.from(items)
-    .map((el) => patern.posts.reduce((acc, tagName) => (
-      { ...acc, [tagName]: mapping.posts[tagName](el)(tagName) }
-    ), {}));
-  // { idFeed: '', id: '', title: '', description: '', link: '' };
-  // return domFromXML;
-  return { feeds, posts };
+    .map((el) => ({
+      title: el.querySelector('title').textContent,
+      description: el.querySelector('description').textContent,
+      link: el.querySelector('link').textContent,
+    }));
+
+  return { feed, posts };
 };
